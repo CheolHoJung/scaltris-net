48cd9a56a47746055f646d769fd95cc742d4f319
CheolHoJung <fnel159@naver.com>
2017-10-28 20:58:56 +0900

    Block test

---
+++ test/scaltris_net/block/BlockTest.scala
@@ -0,0 +1,26 @@
+package scaltris_net.block
+
+import org.scalatest._
+import java.awt.Color
+
+
+class BlockTest extends FlatSpec with Matchers {
+  
+	it should "have color" in {
+		Block.getBlockColor(Block.T) should not equal (null)
+		Block.getBlockColor(Block.T) shouldBe a [Color]
+	}
+	
+	"nextBlock" should "a random element in Block.values that not Block.EMPTY." in {
+		for(i <- 0 until 10) {
+			Block.values should not be (Block.EMPTY)
+			Block.values should contain (Block.nextBlock)
+		}
+	}
+	
+	"Block.EMPTY instance positions" should "be empty array" in {
+		val emptyBlock = Block.EMPTY
+		Block.getPositions(emptyBlock) shouldBe empty
+	}
+}
+  
